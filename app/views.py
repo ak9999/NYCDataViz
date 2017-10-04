0aee86e681eca6ea592235aba6a88bf6bc5f50e2
Abdullah Khan <ajkhan4096@gmail.com>
2017-10-04 01:43:11 -0400

    Return 500K records from the last 6 weeks.

--- app/views.py
+++ app/views.py
@@ -3,17 +3,12 @@ from app import app
 from flask import render_template
 from flask import Response
 
-from sodapy import Socrata
-
 from dateutil.relativedelta import *
 from dateutil.parser import *
 
 import requests
 
-import json
 from datetime import *
-
-client = Socrata('data.cityofnewyork.us', None)
 
 @app.route('/')
 @app.route('/index.html')
@@ -26,38 +21,33 @@ def index():
 
 @app.route('/query/')
 def request():
-    # Refer to globally defined client variable.
-    global client
     # These are the desired columns:
     # ['Latitude', 'Longitude', 'Created Date', 'Agency', 'Agency Name', 'Complaint Type', 'Descriptor']
-
     columns = "latitude,longitude,created_date,agency,agency_name,complaint_type,descriptor"
 
     # Get recent service requests (last 6 weeks)
     today = date.today()
     six_weeks_ago = today - relativedelta(weeks=6)
+    # Convert datetimes into Floating Timestamps for use with Socrata.
     today = today.strftime('%Y-%m-%d') + 'T00:00:00'
     six_weeks_ago = six_weeks_ago.strftime('%Y-%m-%d') + 'T00:00:00'
-    # today = today.strftime('%Y-%m-%d')
-    # six_weeks_ago = six_weeks_ago.strftime('%Y-%m-%d')
+    # Print out the values.
     print(today)
     print(six_weeks_ago)
-    # query_data = client.get('fhrw-4uyv', select=columns,
-    #                         where="created_date between {} and {}".format(six_weeks_ago, today))
 
-    # r = requests.get(
-    #     "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?" +
-    #     "$where=created_date between \'{}\' and \'{}\',".format(six_weeks_ago, today) +
-    #     "$select={}".format(columns)
-    # )
-
+    '''
+    GET request on Socrata's API.
+    First part is the data set we're using.
+    $limit is set to 500K, because we want 500K records.
+    $select will select the columns we want, as defined earlier.
+    $where allows us to choose the time frame. In this case it's 6 weeks.
+    '''
     r = requests.get(
-        "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?$query=" +
-        "SELECT {} WHERE".format(columns) +
-        " created_date between \'{}\' and \'{}\'".format(six_weeks_ago, today)
+        "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?" +
+        "$limit=500000&$select={}&".format(columns) +
+        "$where=created_date between \'{}\' and \'{}\'".format(six_weeks_ago, today)
     )
 
     # Create the response
-    # response = Response(response=json.loads(query_data), status=200, mimetype='application/json')
     response = Response(response=r, status=200, mimetype='application/json')
     return response
