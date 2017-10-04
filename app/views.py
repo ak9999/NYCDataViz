d0f2a3edf4a5b6317fe37a8ce61c58c947281f55
Abdullah Khan <ajkhan4096@gmail.com>
2017-10-04 01:30:16 -0400

    Return data from last 6 weeks

--- app/views.py
+++ app/views.py
@@ -2,9 +2,16 @@ from app import app
 
 from flask import render_template
 from flask import Response
+
 from sodapy import Socrata
 
+from dateutil.relativedelta import *
+from dateutil.parser import *
+
+import requests
+
 import json
+from datetime import *
 
 client = Socrata('data.cityofnewyork.us', None)
 
@@ -23,9 +30,34 @@ def request():
     global client
     # These are the desired columns:
     # ['Latitude', 'Longitude', 'Created Date', 'Agency', 'Agency Name', 'Complaint Type', 'Descriptor']
+
     columns = "latitude,longitude,created_date,agency,agency_name,complaint_type,descriptor"
-    query_data = client.get('fhrw-4uyv', limit=200, select=columns)
+
+    # Get recent service requests (last 6 weeks)
+    today = date.today()
+    six_weeks_ago = today - relativedelta(weeks=6)
+    today = today.strftime('%Y-%m-%d') + 'T00:00:00'
+    six_weeks_ago = six_weeks_ago.strftime('%Y-%m-%d') + 'T00:00:00'
+    # today = today.strftime('%Y-%m-%d')
+    # six_weeks_ago = six_weeks_ago.strftime('%Y-%m-%d')
+    print(today)
+    print(six_weeks_ago)
+    # query_data = client.get('fhrw-4uyv', select=columns,
+    #                         where="created_date between {} and {}".format(six_weeks_ago, today))
+
+    # r = requests.get(
+    #     "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?" +
+    #     "$where=created_date between \'{}\' and \'{}\',".format(six_weeks_ago, today) +
+    #     "$select={}".format(columns)
+    # )
+
+    r = requests.get(
+        "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?$query=" +
+        "SELECT {} WHERE".format(columns) +
+        " created_date between \'{}\' and \'{}\'".format(six_weeks_ago, today)
+    )
 
     # Create the response
-    response = Response(response=json.dumps(query_data), status=200, mimetype='application/json')
+    # response = Response(response=json.loads(query_data), status=200, mimetype='application/json')
+    response = Response(response=r, status=200, mimetype='application/json')
     return response
