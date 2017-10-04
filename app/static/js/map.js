8bc6afb7771185699c18b330dd0dcd71c475f996
Abdullah Khan <ajkhan4096@gmail.com>
2017-10-04 19:12:32 -0400

    Specifies timezone to avoid datetime errors. Changed delta to 3 days.
    Should be ready for deployment on Heroku.

---
+++ app/static/js/map.js
@@ -0,0 +1,173 @@
+// This example requires the Visualization library. Include the libraries=visualization
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=visualization">
+
+
+
+        var map;
+        var heatmap;
+
+        function ready(fn){
+            if(document.attachEvent ? document.readyState === "complete" : document.readyState != "loading"){
+                fn();
+            }else{
+                document.addEventListener('DOMContentLoaded',fn);
+            }
+        }
+
+        function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 13,
+                center: {
+                    lat: 40.730815,
+                    lng: -73.997471
+                },
+            mapTypeId: google.maps.MapTypeId.MAP,
+            disableDefaultUI: true
+            });
+
+            var request = new XMLHttpRequest();
+            request.open('GET', '/query', true);
+
+            request.onload = function() {
+              if (request.status >= 200 && request.status < 400) {
+                    // Success!
+                    var data = JSON.parse(request.responseText);
+                    let result = [];
+                    for (var i = 0; i < data.length; i++) {
+                        result.push(new google.maps.LatLng(data[i].latitude, data[i].longitude));
+                    }
+                    console.log(result);
+                    heatmap = new google.maps.visualization.HeatmapLayer({
+                        data: result,
+                        map: map
+                    });
+              } 
+            };
+            request.onerror = function() {
+            // There was a connection error of some sort
+            };  
+
+            request.send();
+        }   
+
+        ready(initMap);
+
+
+    function recv_data(data_url, callback) {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() {
+            if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+                callback(xmlHttp.responseText);
+            }
+        };
+        xmlHttp.open("GET", data_url, true); // asynchronous request
+        xmlHttp.send(null);
+    }
+
+    function toggleHeatmap() {
+        heatmap.setMap(heatmap.getMap() ? null : map);
+    }
+
+
+function changeGradient() {
+    var gradient = [
+        'rgba(0, 255, 255, 0)',
+        'rgba(0, 255, 255, 1)',
+        'rgba(0, 191, 255, 1)',
+        'rgba(0, 127, 255, 1)',
+        'rgba(0, 63, 255, 1)',
+        'rgba(0, 0, 255, 1)',
+        'rgba(0, 0, 223, 1)',
+        'rgba(0, 0, 191, 1)',
+        'rgba(0, 0, 159, 1)',
+        'rgba(0, 0, 127, 1)',
+        'rgba(63, 0, 91, 1)',
+        'rgba(127, 0, 63, 1)',
+        'rgba(191, 0, 31, 1)',
+        'rgba(255, 0, 0, 1)'
+    ]
+    heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+}
+
+function changeRadius() {
+    heatmap.set('radius', heatmap.get('radius') ? null : 20);
+}
+
+function changeOpacity() {
+    heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+}
+
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
+
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+function initAutocomplete() {
+    /* map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+    lat: -33.8688,
+    lng: 151.2195
+},
+zoom: 13,
+mapTypeId: google.maps.MapTypeId.ROADMAP
+}); */
+
+// Create the search box and link it to the UI element.
+var input = document.getElementById('pac-input');
+var searchBox = new google.maps.places.SearchBox(input);
+// map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+// Bias the SearchBox results towards current map's viewport.
+map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+});
+
+var markers = [];
+// Listen for the event fired when the user selects a prediction and retrieve
+// more details for that place.
+searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length === 0) {
+        return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+        marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+        var icon = {
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+        };
+
+        // Create a marker for each place.
+        markers.push(new google.maps.Marker({
+            map: map,
+            icon: icon,
+            title: place.name,
+            position: place.geometry.location
+        }));
+
+        if (place.geometry.viewport) {
+            // Only geocodes have viewport.
+            bounds.union(place.geometry.viewport);
+        } else {
+            bounds.extend(place.geometry.location);
+        }
+    });
+    map.fitBounds(bounds);
+});
+}
--- run.py
+++ run.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3
 from app import app
-app.run()
+app.run(debug=True, use_reloader=True)
