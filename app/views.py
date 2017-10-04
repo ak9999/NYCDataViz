31c8e6eb002452c3ded97cb3ac6346e61b735d0d
Abdullah Khan <ajkhan4096@gmail.com>
2017-10-03 23:20:06 -0400

    Return JSON of basic query.

--- app/views.py
+++ app/views.py
@@ -2,9 +2,9 @@ from app import app
 
 from flask import render_template
 from flask import Response
-from flask import jsonify
-
 from sodapy import Socrata
+
+import json
 
 client = Socrata('data.cityofnewyork.us', None)
 
@@ -17,9 +17,15 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/demo/')
-def request_demo():
+@app.route('/query/')
+def request():
+    # Refer to globally defined client variable.
     global client
-    two_points = client.get('fhrw-4uyv', limit=2)
-    response = Response(response=jsonify(two_points), status=200)
+    # These are the desired columns:
+    # ['Latitude', 'Longitude', 'Created Date', 'Agency', 'Agency Name', 'Complaint Type', 'Descriptor']
+    columns = "latitude,longitude,created_date,agency,agency_name,complaint_type,descriptor"
+    query_data = client.get('fhrw-4uyv', limit=200, select=columns)
+
+    # Create the response
+    response = Response(response=json.dumps(query_data), status=200, mimetype='application/json')
     return response
