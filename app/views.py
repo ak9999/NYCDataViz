b5f5be1d85049b9a6be53144ae9f5d532017c972
Abdullah Khan <ajkhan4096@gmail.com>
2017-10-29 14:50:42 -0400

    Specifies timezone to avoid datetime errors. Changed delta to 3 days.
    Should be ready for deployment on Heroku.

--- app/views.py
+++ app/views.py
@@ -3,12 +3,12 @@ from app import app
 from flask import render_template
 from flask import Response
 
+import pytz
+from datetime import *
 from dateutil.relativedelta import *
 from dateutil.parser import *
 
 import requests
-
-from datetime import *
 import os
 
 @app.route('/')
@@ -23,27 +23,29 @@ def index():
 @app.route('/query/')
 def request():
     # These are the desired columns:
-    # ['Latitude', 'Longitude', 'Created Date', 'Agency', 'Agency Name', 'Complaint Type', 'Descriptor']
-    columns = "latitude,longitude,created_date,agency,agency_name,complaint_type,descriptor"
+    # ['Unique Key', 'Latitude', 'Longitude', 'Created Date', 'Agency', 'Agency Name', 'Complaint Type', 'Descriptor']
+    columns = "unique_key,latitude,longitude,created_date,agency,agency_name,complaint_type,descriptor"
 
-    # Get recent service requests (last 6 weeks)
-    today = date.today()
-    six_weeks_ago = today - relativedelta(days=1)
+    # Get recent service requests
+    eastern_tz = pytz.timezone('US/Eastern')  # Generate time zone from string.
+    today = datetime.now()  # Generate datetime object right now.
+    today = today.astimezone(eastern_tz)  # Convert today to new datetime
+    time_delta = today - relativedelta(days=3)
     # Convert datetimes into Floating Timestamps for use with Socrata.
     today = today.strftime('%Y-%m-%d') + 'T00:00:00'
-    six_weeks_ago = six_weeks_ago.strftime('%Y-%m-%d') + 'T00:00:00'
+    time_delta = time_delta.strftime('%Y-%m-%d') + 'T00:00:00'
 
     '''
     GET request on Socrata's API.
     First part is the data set we're using.
-    $limit is set to 500K, because we want 500K records.
+    $limit is set to the maximum number of records we want.
     $select will select the columns we want, as defined earlier.
     $where allows us to choose the time frame. In this case it's 6 weeks.
     '''
     r = requests.get(
         "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?" +
         "$limit=500000&$select={}&".format(columns) +
-        "$where=created_date between \'{}\' and \'{}\' and longitude IS NOT NULL".format(six_weeks_ago, today)
+        "$where=created_date between \'{}\' and \'{}\' and longitude IS NOT NULL".format(time_delta, today)
     )
 
     # Create the response
