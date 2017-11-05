304e09f801882cf8b5d365e4767da77b01c68723
Abdullah Khan <ajkhan4096@gmail.com>
2017-11-05 16:16:11 -0500

    /query end point now allows filtering for agency and complaint type

--- app/views.py
+++ app/views.py
@@ -1,7 +1,6 @@
 from app import app
 
-from flask import render_template
-from flask import Response
+from flask import render_template, Response, request
 
 import pytz
 from datetime import *
@@ -24,8 +23,8 @@ def map():
     return render_template('map.html')
 
 
-@app.route('/query/')
-def request():
+@app.route('/query')
+def request_data():
     # These are the desired columns:
     # ['Unique Key', 'Latitude', 'Longitude', 'Created Date', 'Agency', 'Agency Name', 'Complaint Type', 'Descriptor']
     columns = "unique_key,latitude,longitude,created_date,agency,agency_name,complaint_type,descriptor"
@@ -35,7 +34,7 @@ def request():
     today = datetime.now()  # Generate datetime object right now.
     today = today.astimezone(eastern_tz)  # Convert today to new datetime
     time_delta = today - relativedelta(days=3)
-    
+
     # Convert datetimes into Floating Timestamps for use with Socrata.
     today = today.strftime('%Y-%m-%d') + 'T00:00:00'
     time_delta = time_delta.strftime('%Y-%m-%d') + 'T00:00:00'
@@ -47,11 +46,16 @@ def request():
     $select will select the columns we want, as defined earlier.
     $where allows us to choose the time frame. In this case it's 6 weeks.
     '''
-    r = requests.get(
-        "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?" +
-        "$limit=500000&$select={}&".format(columns) +
-        "$where=created_date between \'{}\' and \'{}\' and longitude IS NOT NULL".format(time_delta, today)
-    )
+    url = "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?"
+    url += "$limit=10000&$select={}&".format(columns)
+    url += "$where=created_date between \'{}\' and \'{}\' and longitude IS NOT NULL".format(time_delta, today)
+    agency = request.args.get('agency')
+    complaint_type = request.args.get('type')
+    if agency:
+        url += f'&agency={agency}'
+    if complaint_type is not None:
+        url += f'&complaint_type={complaint_type}'
+    r = requests.get(url)
 
     # Create the response
     response = Response(response=r, status=200, mimetype='application/json')
