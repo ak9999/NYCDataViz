7a632f53c68c8d1dd7e41e6973d9b598d46534c3
Jesaiah Prayor <jesaiahprayor@gmail.com>
2017-11-09 08:52:08 -0500

    Ai

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -108,7 +108,64 @@ function initMap() {
         disableDefaultUI: true
     });
 
+    // Auto Complete
+        var input = document.getElementById('pac-input');
 
+        var autocomplete = new google.maps.places.Autocomplete(input);
+
+        // Bind the map's bounds (viewport) property to the autocomplete object,
+        // so that the autocomplete requests use the current map bounds for the
+        // bounds option in the request.
+        autocomplete.bindTo('bounds', map);
+
+        var infowindow = new google.maps.InfoWindow();
+        var infowindowContent = document.getElementById('infowindow-content');
+        infowindow.setContent(infowindowContent);
+        // whats this??
+        
+        var marker = new google.maps.Marker({
+          map: map,
+          anchorPoint: new google.maps.Point(0, -29)
+        });
+
+        autocomplete.addListener('place_changed', function() {
+          infowindow.close();
+          marker.setVisible(false);
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+          }
+
+          // If the place has a geometry, then present it on a map.
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+          }
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
+
+          var address = '';
+          if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
+
+          infowindowContent.children['place-icon'].src = place.icon;
+          infowindowContent.children['place-name'].textContent = place.name;
+          infowindowContent.children['place-address'].textContent = address;
+          infowindow.open(map, marker);
+        });
+
+
+    // end of Auto Complete
 
     var request = new XMLHttpRequest();
     request.open('GET', '/query', true);
@@ -294,7 +351,7 @@ function DEPFunction(){
 function allData(){
     heatmap.setMap(null);
     var request = new XMLHttpRequest();
-    request.open('GET', '/query?&agency=DEP', true);
+    request.open('GET', '/query', true);
 
     request.onload = function() {
         if (request.status >= 200 && request.status < 400) {
@@ -345,4 +402,5 @@ window.onclick = function(event) {
     }
   }
 }
+
 
